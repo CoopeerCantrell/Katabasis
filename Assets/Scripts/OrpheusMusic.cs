42f486287b338bf230b1c975a819c2dae18ee4a9
CooperCantrell <coopercantrell56@gmail.com>
2026-09-04 17:47:55 -0500

    music is good

--- Assets/Scripts/OrpheusMusic.cs
+++ Assets/Scripts/OrpheusMusic.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class OrpheusMusic : MonoBehaviour
@@ -15,20 +16,62 @@ public class OrpheusMusic : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            PlayMusic();
+            StartCoroutine(RunFunctionForFiveSeconds());
         }
     }
     void PlayMusic(){
-        hitColliders = Physics2D.OverlapCircleAll(transform.position,radius);
+    
+    hitColliders = Physics2D.OverlapCircleAll(transform.position,radius);
         foreach (var hitCollider in hitColliders) 
     {
             if (hitCollider.CompareTag("Enemy"))
             {
                 GameObject enemy = hitCollider.gameObject;
-                float speeds = enemy.GetComponent<BasicEnemy>().speed;
-                speeds = speeds / 2;
+                float orispeeds = enemy.GetComponent<BasicEnemy>().speed;
+                if(orispeeds != 0){
+                    orispeeds = -10;
+                }
+                
+                enemy.GetComponent<BasicEnemy>().SetSpeed(orispeeds);
+                Debug.Log("set original speed");
+                float speeds = orispeeds /4;
                 enemy.GetComponent<BasicEnemy>().SetSpeed(speeds);
+                Debug.Log("set slower speed");
         }
     }
+    }
+
+    void ResetSpeed(){
+        hitColliders = Physics2D.OverlapCircleAll(transform.position,radius + 2);
+        foreach (var hitCollider in hitColliders) 
+    {
+            if (hitCollider.CompareTag("Enemy"))
+            {
+                GameObject enemy = hitCollider.gameObject;
+                float orispeeds = enemy.GetComponent<BasicEnemy>().speed;
+                if(orispeeds != 0)
+                    orispeeds = -10;
+                enemy.GetComponent<BasicEnemy>().SetSpeed(orispeeds);
+                Debug.Log("set original speed after leaving area of ability");
+        }
+    }
+    }
+
+    IEnumerator RunFunctionForFiveSeconds()
+    {
+        float elapsedTime = 0f;
+        float duration = 5f;
+
+        while (elapsedTime < duration)
+        {
+            ResetSpeed();
+            PlayMusic();
+            
+            elapsedTime += Time.deltaTime; 
+            yield return null; 
+        }
+        
+        ResetSpeed();
+        Debug.Log("5 seconds are up! Stopped calling the function.");
     }
 }
